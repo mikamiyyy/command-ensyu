e2980b8ae0319bc9cb312a3cf851dae3149e387b
mikami <i.mikami.sys22@morijyobi.ac.jp>
2022-11-10 10:43:36 +0900

    Main.javaを修正

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<2; i++){
 		System.out.print("身長：");
 		double height = sc.nextDouble();
 		System.out.print("体重：");
 		double weight = sc.nextDouble();
 		double bmi = weight/Math.pow((height/100), 2);
 		System.out.println("BMIは"+Math.floor(bmi*100)/100+"です");
+		}
 		sc.close();
 	}
 }
